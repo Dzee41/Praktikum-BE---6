fbe80f8fdc7bbbeb58a44693de34678906519247
Dzee41 <zei.dhe21@gmail.com>
2023-11-03 09:26:18 +0700

    pertemuan 5

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 use PhpParser\Node\Stmt\Echo_;
 
 /*
@@ -31,3 +32,9 @@ Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 #method DELETE
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+#Route Students
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
